bf8af3890627c1eb28e1b73e4a774aa22966fca5
bougzy <yubeespecial@gmail.com>
2024-09-13 07:32:13 +0100

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const NavigationBar = () => {
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/">Apartment Rent App</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Apartment Rent Application</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
